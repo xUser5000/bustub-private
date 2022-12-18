70b2ce2cd1d934f3c07830c9fcf3a55fbff527b7
Abdallah Ahmed <abdallahar1974@gmail.com>
2022-12-18 03:10:37 +0200

    Implement fine-grained locking in the buffer pool

--- src/buffer/buffer_pool_manager_instance.cpp
+++ src/buffer/buffer_pool_manager_instance.cpp
@@ -25,6 +25,7 @@ BufferPoolManagerInstance::BufferPoolManagerInstance(size_t pool_size, DiskManag
   pages_ = new Page[pool_size_];
   page_table_ = new ExtendibleHashTable<page_id_t, frame_id_t>(bucket_size_);
   replacer_ = new LRUKReplacer(pool_size, replacer_k);
+  local_latches_ = std::vector<std::mutex>(pool_size_);
 
   // Initially, every page is in the free list.
   for (size_t i = 0; i < pool_size_; ++i) {
@@ -39,44 +40,45 @@ BufferPoolManagerInstance::~BufferPoolManagerInstance() {
 }
 
 auto BufferPoolManagerInstance::NewPgImp(page_id_t *page_id) -> Page * {
-  std::scoped_lock<std::mutex> lock(latch_);
   frame_id_t frame_id;
+  global_latch_.lock();
+  *page_id = AllocatePage();
   if (!free_list_.empty()) {
-    frame_id = free_list_.back();
-    free_list_.pop_back();
+    frame_id = free_list_.front();
+    free_list_.pop_front();
   } else {
     if (replacer_->Size() > 0) {
       replacer_->Evict(&frame_id);
       page_table_->Remove(pages_[frame_id].GetPageId());
     } else {
+      global_latch_.unlock();
       return nullptr;
     }
   }
+  replacer_->RecordAccess(frame_id);
+  replacer_->SetEvictable(frame_id, false);
+  page_table_->Insert(*page_id, frame_id);
 
-  *page_id = AllocatePage();
+  std::scoped_lock<std::mutex> local_lock(local_latches_[frame_id]);
+  global_latch_.unlock();
 
   if (pages_[frame_id].IsDirty()) {
     disk_manager_->WritePage(pages_[frame_id].GetPageId(), pages_[frame_id].GetData());
   }
-
   pages_[frame_id].ResetMemory();
   pages_[frame_id].page_id_ = *page_id;
   pages_[frame_id].is_dirty_ = false;
   pages_[frame_id].pin_count_ = 1;
 
-  replacer_->RecordAccess(frame_id);
-  replacer_->SetEvictable(frame_id, false);
-
-  page_table_->Insert(*page_id, frame_id);
-
   return pages_ + frame_id;
 }
 
 auto BufferPoolManagerInstance::FetchPgImp(page_id_t page_id) -> Page * {
-  std::scoped_lock<std::mutex> lock(latch_);
   frame_id_t frame_id;
+  std::unique_lock<std::mutex> global_lock(global_latch_);
   if (page_table_->Find(page_id, frame_id)) {
     replacer_->RecordAccess(frame_id);
+    std::scoped_lock<std::mutex> local_lock(local_latches_[frame_id]);
     if (pages_[frame_id].pin_count_ == 0) {
       replacer_->SetEvictable(frame_id, false);
     }
@@ -84,8 +86,8 @@ auto BufferPoolManagerInstance::FetchPgImp(page_id_t page_id) -> Page * {
     return pages_ + frame_id;
   }
   if (!free_list_.empty()) {
-    frame_id = free_list_.back();
-    free_list_.pop_back();
+    frame_id = free_list_.front();
+    free_list_.pop_front();
   } else {
     if (replacer_->Size() > 0) {
       replacer_->Evict(&frame_id);
@@ -94,34 +96,37 @@ auto BufferPoolManagerInstance::FetchPgImp(page_id_t page_id) -> Page * {
       return nullptr;
     }
   }
+  replacer_->RecordAccess(frame_id);
+  replacer_->SetEvictable(frame_id, false);
+  page_table_->Insert(page_id, frame_id);
+
+  std::scoped_lock<std::mutex> local_lock(local_latches_[frame_id]);
+  global_lock.unlock();
 
   if (pages_[frame_id].IsDirty()) {
     disk_manager_->WritePage(pages_[frame_id].GetPageId(), pages_[frame_id].GetData());
   }
-
   disk_manager_->ReadPage(page_id, pages_[frame_id].GetData());
   pages_[frame_id].page_id_ = page_id;
   pages_[frame_id].is_dirty_ = false;
   pages_[frame_id].pin_count_ = 1;
 
-  replacer_->RecordAccess(frame_id);
-  replacer_->SetEvictable(frame_id, false);
-
-  page_table_->Insert(page_id, frame_id);
-
   return pages_ + frame_id;
 }
 
 auto BufferPoolManagerInstance::UnpinPgImp(page_id_t page_id, bool is_dirty) -> bool {
-  std::scoped_lock<std::mutex> lock(latch_);
   frame_id_t frame_id;
+  std::unique_lock<std::mutex> global_lock(global_latch_);
   if (!page_table_->Find(page_id, frame_id)) {
     return false;
   }
+
+  std::scoped_lock<std::mutex> local_lock(local_latches_[frame_id]);
+  global_lock.unlock();
+
   if (pages_[frame_id].GetPinCount() == 0) {
     return false;
   }
-
   pages_[frame_id].pin_count_--;
   if (pages_[frame_id].GetPinCount() == 0) {
     replacer_->SetEvictable(frame_id, true);
@@ -133,11 +138,14 @@ auto BufferPoolManagerInstance::UnpinPgImp(page_id_t page_id, bool is_dirty) -> 
 }
 
 auto BufferPoolManagerInstance::FlushPgImp(page_id_t page_id) -> bool {
-  std::scoped_lock<std::mutex> lock(latch_);
   frame_id_t frame_id;
+  std::unique_lock<std::mutex> global_lock(global_latch_);
   if (!page_table_->Find(page_id, frame_id)) {
     return false;
   }
+  std::scoped_lock<std::mutex> local_lock(local_latches_[frame_id]);
+  global_lock.unlock();
+
   disk_manager_->WritePage(page_id, pages_[frame_id].GetData());
   pages_[frame_id].is_dirty_ = false;
   return true;
@@ -146,32 +154,34 @@ auto BufferPoolManagerInstance::FlushPgImp(page_id_t page_id) -> bool {
 void BufferPoolManagerInstance::FlushAllPgsImp() {
   for (size_t i = 0; i < pool_size_; i++) {
     if (pages_[i].page_id_ != INVALID_PAGE_ID) {
+      local_latches_[i].lock();
       disk_manager_->WritePage(pages_[i].GetPageId(), pages_[i].GetData());
       pages_[i].is_dirty_ = false;
+      local_latches_[i].unlock();
     }
   }
 }
 
 auto BufferPoolManagerInstance::DeletePgImp(page_id_t page_id) -> bool {
-  std::scoped_lock<std::mutex> lock(latch_);
   frame_id_t frame_id;
+  std::unique_lock<std::mutex> global_lock(global_latch_);
   if (!page_table_->Find(page_id, frame_id)) {
     return true;
   }
+  std::scoped_lock<std::mutex> local_lock(local_latches_[frame_id]);
   if (pages_[frame_id].GetPinCount() != 0) {
     return false;
   }
   page_table_->Remove(page_id);
   replacer_->Remove(frame_id);
   free_list_.push_back(frame_id);
+  DeallocatePage(page_id);
+  global_lock.unlock();
 
   pages_[frame_id].ResetMemory();
   pages_[frame_id].page_id_ = INVALID_PAGE_ID;
   pages_[frame_id].pin_count_ = 0;
   pages_[frame_id].is_dirty_ = false;
-
-  DeallocatePage(page_id);
-
   return true;
 }
 
--- src/include/buffer/buffer_pool_manager_instance.h
+++ src/include/buffer/buffer_pool_manager_instance.h
@@ -14,7 +14,7 @@
 
 #include <list>
 #include <mutex>  // NOLINT
-#include <unordered_map>
+#include <vector>
 
 #include "buffer/buffer_pool_manager.h"
 #include "buffer/lru_k_replacer.h"
@@ -160,7 +160,8 @@ class BufferPoolManagerInstance : public BufferPoolManager {
   /** List of free frames that don't have any pages on them. */
   std::list<frame_id_t> free_list_;
   /** This latch protects shared data structures. We recommend updating this comment to describe what it protects. */
-  std::mutex latch_;
+  std::mutex global_latch_;
+  std::vector<std::mutex> local_latches_;
 
   /**
    * @brief Allocate a page on disk. Caller should acquire the latch before calling this function.
