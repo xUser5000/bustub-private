5521de53125fac90c9e2df1bfdb3f65a5853bbb0
xUser5000 <abdallahar1974@gmail.com>
2023-02-14 16:43:40 +0200

    feat(p3): Implemented insert executor

--- src/include/execution/executors/insert_executor.h
+++ src/include/execution/executors/insert_executor.h
@@ -57,6 +57,8 @@ class InsertExecutor : public AbstractExecutor {
  private:
   /** The insert plan node to be executed*/
   const InsertPlanNode *plan_;
+  std::unique_ptr<AbstractExecutor> child_executor_;
+  bool used_;
 };
 
 }  // namespace bustub
