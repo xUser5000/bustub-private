22c149c2b0f713695e451f6109a381c962ff2c7f
xUser5000 <abdallahar1974@gmail.com>
2023-02-28 12:06:32 +0200

    feat(p3): Implemented top n executor and optimzer rule

--- src/optimizer/sort_limit_as_topn.cpp
+++ src/optimizer/sort_limit_as_topn.cpp
@@ -1,10 +1,25 @@
+#include "execution/plans/limit_plan.h"
+#include "execution/plans/sort_plan.h"
+#include "execution/plans/topn_plan.h"
 #include "optimizer/optimizer.h"
 
 namespace bustub {
 
 auto Optimizer::OptimizeSortLimitAsTopN(const AbstractPlanNodeRef &plan) -> AbstractPlanNodeRef {
-  // TODO(student): implement sort + limit -> top N optimizer rule
-  return plan;
+  std::vector<AbstractPlanNodeRef> children;
+  for (const auto &child : plan->GetChildren()) {
+    children.emplace_back(OptimizeSortLimitAsTopN(child));
+  }
+  auto optimized_plan = plan->CloneWithChildren(std::move(children));
+  if (optimized_plan->GetType() == PlanType::Limit && !optimized_plan->GetChildren().empty() &&
+      optimized_plan->GetChildren()[0]->GetType() == PlanType::Sort) {
+    const auto &limit_plan = dynamic_cast<const LimitPlanNode &>(*optimized_plan);
+    const auto &sort_plan = dynamic_cast<const SortPlanNode *>(limit_plan.GetChildPlan().get());
+    auto topn_plan = std::make_shared<TopNPlanNode>(limit_plan.output_schema_, sort_plan->GetChildPlan(),
+                                                    sort_plan->GetOrderBy(), limit_plan.GetLimit());
+    return topn_plan;
+  }
+  return optimized_plan;
 }
 
 }  // namespace bustub
